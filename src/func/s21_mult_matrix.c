c481f3a60180f62b25f54516d41fdf6f361a4cea
fatkheev <fatkheev@gmail.com>
2023-08-07 19:23:28 +0300

    - calc det inv trans

--- src/func/s21_mult_matrix.c
+++ src/func/s21_mult_matrix.c
@@ -4,7 +4,11 @@ int s21_mult_matrix(matrix_t *A, matrix_t *B, matrix_t *result) {
   int return_code = 0;
 
   if (A == NULL || B == NULL || result == NULL || A->columns != B->rows) {
-    return_code = (A == NULL || B == NULL || result == NULL) ? 1 : 2;
+    if (A == NULL || B == NULL || result == NULL) {
+      return_code = 1;
+    } else {
+      return_code = 2;
+    }
   } else if (s21_create_matrix(A->rows, B->columns, result) == 0) {
     for (int i = 0; i < A->rows; i++) {
       for (int j = 0; j < B->columns; j++) {
